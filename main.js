0cb9adda77f3ba859e7aeb60e865cb746e522b31
Rscar <scarboro.reid@gmail.com>
2015-01-13 22:18:48 -0500

    boop

--- main.js
+++ main.js
@@ -45,6 +45,10 @@
         var newEmoji = Object.create(currentEmoji);
         var siblings = [].slice.call(event.target.parentNode.children);
         newEmoji.note = siblings.indexOf(event.target);
+        newEmoji.image = document.createElement("img");
+        newEmoji.image.className = "emojiNote";
+        newEmoji.image.src = newEmoji.imageURL;
+        event.target.appendChild(newEmoji.image);
         console.log(newEmoji.note);
         musicGrid.push(newEmoji);
         newEmoji.playSound();
@@ -55,7 +59,9 @@
       soundFiles.forEach(function(fileName) {
         key = fileName.split(".")[0];
         var emojiObj = Object.create(EmojiSound);
+        emojiObj.imageURL = "emoji/poo.png";
         emojiObj.loadSound(fileName);
+        emojiDict[key] = emojiObj;
       });
     }
 
